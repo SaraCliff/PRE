9668a5a79e2bef79c9f17161c0e60f295ef99ac7
SaraCliff <sara.soto.henares@estudiantat.upc.edu>
2024-06-12 19:26:15 +0200

    ramas juntadas

--- Pygame/two_player_screen.py
+++ Pygame/two_player_screen.py
@@ -25,48 +25,49 @@ def two_player_screen(screen, personaje):
     ]
 
     images = [pygame.image.load(path) for path in image_paths]
+    database = Database('userdata.db')
 
     buttons = [
         Button(image=pygame.transform.scale(images[0], (125, 150)),
                pos=(WIDTH // 4 - 100, HEIGHT // 3 + 100),
-               text_input="", font=get_font(50), base_color="#d7fcd4", hovering_color="White", image_path=images[0]),
+               text_input="", font=get_font(50), base_color="#d7fcd4", hovering_color="White", image_path=image_paths[0]),
         Button(image=pygame.transform.scale(images[1], (125, 150)),
                pos=(WIDTH // 4 - 100, HEIGHT // 2 + 200),
-               text_input="", font=get_font(35), base_color="#d7fcd4", hovering_color="White",image_path=images[1]),
+               text_input="", font=get_font(35), base_color="#d7fcd4", hovering_color="White", image_path=image_paths[1]),
         Button(image=pygame.transform.scale(images[2], (125, 150)),
                pos=(WIDTH // 4 + 100, HEIGHT // 3 + 100),
-               text_input="", font=get_font(50), base_color="#d7fcd4", hovering_color="White",image_path=images[2]),
+               text_input="", font=get_font(50), base_color="#d7fcd4", hovering_color="White", image_path=image_paths[2]),
         Button(image=pygame.transform.scale(images[3], (125, 150)),
                pos=(WIDTH // 4 + 100, HEIGHT // 2 + 200),
-               text_input="", font=get_font(35), base_color="#d7fcd4", hovering_color="White",image_path=images[3]),
+               text_input="", font=get_font(35), base_color="#d7fcd4", hovering_color="White", image_path=image_paths[3]),
         Button(image=pygame.transform.scale(images[4], (125, 150)),
                pos=(WIDTH // 4 + 300, HEIGHT // 3 + 100),
-               text_input="", font=get_font(50), base_color="#d7fcd4", hovering_color="White",image_path=images[4]),
+               text_input="", font=get_font(50), base_color="#d7fcd4", hovering_color="White", image_path=image_paths[4]),
         Button(image=pygame.transform.scale(images[5], (125, 150)),
                pos=(WIDTH // 4 + 300, HEIGHT // 2 + 200),
-               text_input="", font=get_font(35), base_color="#d7fcd4", hovering_color="White",image_path=images[5])
+               text_input="", font=get_font(35), base_color="#d7fcd4", hovering_color="White", image_path=image_paths[5])
     ]
 
     ready_button = Button(image=pygame.transform.scale(images[0], (200, 50)),
                           pos=(WIDTH // 4 + 800, HEIGHT // 2 + 310),
                           text_input="READY", font=get_font(15),
-                          base_color="#d7fcd4", hovering_color="White",image_path=images[0])
+                          base_color="#d7fcd4", hovering_color="White", image_path=image_paths[0])
 
     player_buttons = [
         Button(image=pygame.transform.scale(images[0], (200, 60)),
                pos=(WIDTH // 4 + 515, HEIGHT // 2 + 250),
                text_input="PLAYER 1", font=get_font(15),
-               base_color="#d7fcd4", hovering_color="White",image_path=images[0]),
+               base_color="#d7fcd4", hovering_color="White", image_path=image_paths[0]),
         Button(image=pygame.transform.scale(images[0], (200, 60)),
                pos=(WIDTH // 4 + 815, HEIGHT // 2 + 250),
                text_input="PLAYER 2", font=get_font(15),
-               base_color="#d7fcd4", hovering_color="White",image_path=images[0])
+               base_color="#d7fcd4", hovering_color="White", image_path=image_paths[0])
     ]
 
     back_button = Button(image=pygame.transform.scale(images[0], (150, 50)),
                          pos=(WIDTH // 4 - 200, HEIGHT // 2 + 310),
                          text_input="BACK", font=get_font(15),
-                         base_color="#d7fcd4", hovering_color="White",image_path=images[0])
+                         base_color="#d7fcd4", hovering_color="White", image_path=image_paths[0])
 
     log_out_BUTTON = Button(image=pygame.transform.scale(pygame.image.load("assets/Options Rect.png"), (150, 70)),
                             pos=(1150, 50),
@@ -79,7 +80,7 @@ def two_player_screen(screen, personaje):
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         selected_images = [personaje.player1, personaje.player2]
-        database = Database('userdata.db')
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -88,18 +89,24 @@ def two_player_screen(screen, personaje):
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if ready_button.checkForInput(MENU_MOUSE_POS):
-                    how_to_play_screen(SCREEN)
+                    if personaje.player1 is not None and personaje.player2 is not None:
+                        personaje.save_selected_character()
+                        personaje.save_selected_character2()
+                        how_to_play_screen(SCREEN)
                 if back_button.checkForInput(MENU_MOUSE_POS):
-                    select_player_mode_screen(SCREEN,personaje)
+                    select_player_mode_screen(SCREEN, personaje)
                 if log_out_BUTTON.checkForInput(MENU_MOUSE_POS):
                     database.logout()
                     main_menu(SCREEN)
+                for button in buttons:
+                    if button.checkForInput(MENU_MOUSE_POS):
+                        personaje.select_character(button)
                 for i, button in enumerate(player_buttons):
                     if button.checkForInput(MENU_MOUSE_POS):
                         if i == 0:  # Si se presiona el botón PLAYER 1
-                            personaje.player1 = personaje.select_character(button)
+                            personaje.update_player1()
                         elif i == 1:  # Si se presiona el botón PLAYER 2
-                            personaje.player2 = personaje.select_character(button)
+                            personaje.update_player2()
 
         text_drawer = TextDrawer(screen)
         text_drawer.draw_text("BEAT BLAST", get_font(100), (186, 85, 211),
@@ -128,8 +135,3 @@ if __name__ == "__main__":
     database = Database('userdata.db')
     personaje = Personaje(database)  # Crear una instancia de la clase Personaje sin valores iniciales
     two_player_screen(SCREEN, personaje)
-
-
-
-
-
